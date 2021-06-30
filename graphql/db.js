75f2b72e8ba76b838998e27ff3afd865c1be19c1
JeUpark <genhcode99@gmail.com>
2021-06-30 12:48:25 +0900

    #0.14 Overview to the final API

--- graphql/db.js
+++ graphql/db.js
@@ -1,18 +1,45 @@
 import axios from 'axios'
 
+const api = axios.create({
+  baseURL: 'https://yts-proxy.nomadcoders1.now.sh/',
+})
+
 export const getMovies = async (limit, rating) => {
   const {
     data: {
       data: { movies },
     },
-  } = await axios.get(
-    'https://yts-proxy.nomadcoders1.now.sh/list_movies.json',
-    {
-      params: {
-        limit: limit,
-        minimum_rating: rating,
-      },
+  } = await api.get('list_movies.json', {
+    params: {
+      limit: limit,
+      minimum_rating: rating,
     },
-  )
+  })
+  return movies
+}
+
+export const getMovie = async (id) => {
+  const {
+    data: {
+      data: { movie },
+    },
+  } = await api.get('movie_details.json', {
+    params: {
+      movie_id: id,
+    },
+  })
+  return movie
+}
+
+export const getSuggestions = async (id) => {
+  const {
+    data: {
+      data: { movies },
+    },
+  } = await api.get('movie_suggestions.json', {
+    params: {
+      movie_id: id,
+    },
+  })
   return movies
 }
